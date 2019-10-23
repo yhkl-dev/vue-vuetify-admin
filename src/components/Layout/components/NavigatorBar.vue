b85bae0db4d8b3c3f50d246bccd6f9eb8e4f784a
yhkl-dev <kaiyang939325@gmail.com>
2019-10-23 22:21:31 +0800

    update

--- src/components/Layout/components/NavigatorBar.vue
+++ src/components/Layout/components/NavigatorBar.vue
@@ -1,42 +1,44 @@
 <template>
-  <v-navigation-drawer v-model="drawer" app>
-    <v-list dense>
-      <v-subheader>YHKL</v-subheader>
-      <v-list-item-group
-        v-model="item"
-        color="primary"
-      >
-        <v-list-item
-          v-for="(item, i) in routes"
-          :key="i"
-        >
-          <v-list-item-icon>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.label"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
+  <v-navigation-drawer
+    v-model="sidebar.opened" app>
+
+    <v-subheader inset>YHKL</v-subheader>
+    <v-list>
+      <div v-for="(item, i) in routes"
+           :key="i">
+        <div v-if="item.children.length === 0">
+          <v-list-item link @click="linkToRouter(item.path)">
+            <v-list-item-icon>
+              <i class="material-icons">{{ item.icon }}</i>
+            </v-list-item-icon>
+            <v-list-item-title>{{ item.label }}</v-list-item-title>
+          </v-list-item>
+        </div>
+        <div v-else>
+          <v-list-group
+            v-model="item.show"
+            no-action
+            value="true">
+            <v-icon slot="prependIcon" >{{ item.icon }}</v-icon>
+<!--            <v-icon>mdi-home</v-icon>-->
+            <template v-slot:activator>
+              <v-list-item-title>{{ item.label }}</v-list-item-title>
+            </template>
+            <v-list-item
+              v-for="(it, i) in item.children"
+              v-if="it.show === true"
+              @click="linkToRouter(item.path)"
+              :key="i"
+              >
+              <v-list-item-title v-text="it.label"></v-list-item-title>
+<!--              <v-list-item-icon>-->
+<!--                <i class="material-icons">{{ it.icon }}</i>-->
+<!--              </v-list-item-icon>-->
+            </v-list-item>
+          </v-list-group>
+        </div>
+      </div>
     </v-list>
-
-<!--      <v-list-item >-->
-<!--        <v-list-item-action>-->
-<!--          <v-icon>mdi-home</v-icon>-->
-<!--        </v-list-item-action>-->
-<!--        <v-list-item-content>-->
-<!--          <v-list-item-title>Home</v-list-item-title>-->
-<!--        </v-list-item-content>-->
-<!--      </v-list-item>-->
-<!--      <v-list-item >-->
-<!--        <v-list-item-action>-->
-<!--          <v-icon>mdi-contact-mail</v-icon>-->
-<!--        </v-list-item-action>-->
-<!--        <v-list-item-content>-->
-<!--          <v-list-item-title>Contact</v-list-item-title>-->
-<!--        </v-list-item-content>-->
-<!--      </v-list-item>-->
-
   </v-navigation-drawer>
 
 </template>
@@ -46,34 +48,26 @@ import { mapGetters } from 'vuex'
 
 export default {
   name: 'NavigatorBar',
-  data () {
-    return {
-      item: 0
-    }
-  },
   props: {
     drawer: Boolean
   },
+  data () {
+    return {
+      // item: 0
+      // active: true
+    }
+  },
   computed: {
     ...mapGetters({
+      sidebar: 'sidebar',
       routes: 'menus'
-    }),
-    onRouters () {
-      const array = this.$route.path.split('/')
-      if (array[3]) {
-        return '/' + array[1] + '/' + array[2]
+    })
+  },
+  methods: {
+    linkToRouter (path) {
+      if (this.$route.path !== path) {
+        this.$router.push({ path: path })
       }
-      return this.$route.path
-    },
-    list () {
-      const list = []
-      this.routes.map(v => {
-        if (!v.meta.hidden) {
-          list.push(v)
-        }
-      })
-      console.log(this.$store.getters)
-      return list
     }
   }
 }
